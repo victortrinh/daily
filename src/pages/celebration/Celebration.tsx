26ca44598955bc1020a6388668a230d6bfcc82bf
Victor Trinh <victor.trinh@gsoft.com>
2023-03-03 09:59:00 -0500

    Keep only shoutout

--- src/pages/celebration/Celebration.tsx
+++ src/pages/celebration/Celebration.tsx
@@ -93,13 +93,13 @@ const Celebration = () => {
   const portrait = ['Maxime', 'Etienne', 'Kanika', 'GR', 'Mike', 'Ben', 'Abdoulaye', 'Victor', 'Kevin'];
 
   const prompts = [
-    'Generate a non existent random inspirational quote that will energize my team in Yoda style and attribute it to Yoda',
-    'Generate a non existent random inspirational quote that will energize my team in a pirate voice and attribute it to pirate character',
-    'Generate a non existent random inspirational quote that sounds like a quote from a sci-fi movie and attribute it to a character.',
-    'Generate a non existent random inspirational quote that will revolutionize the tech industry and attribute it to the God of tech',
-    'Generate a non existent random inspirational quote using metaphors and attribute it to a real athlete.',
-    'Generate a home repairs protip from a 90s movie and attribute it to the movie\'s character.',
-    'Start writing an inspirational quote but veer into unspeakable dark eldrich horror right before the end. Attribute it to a comedy movie bad guy.',
+    // 'Generate a non existent random inspirational quote that will energize my team in Yoda style and attribute it to Yoda',
+    // 'Generate a non existent random inspirational quote that will energize my team in a pirate voice and attribute it to pirate character',
+    // 'Generate a non existent random inspirational quote that sounds like a quote from a sci-fi movie and attribute it to a character.',
+    // 'Generate a non existent random inspirational quote that will revolutionize the tech industry and attribute it to the God of tech',
+    // 'Generate a non existent random inspirational quote using metaphors and attribute it to a real athlete.',
+    // 'Generate a home repairs protip from a 90s movie and attribute it to the movie\'s character.',
+    // 'Start writing an inspirational quote but veer into unspeakable dark eldrich horror right before the end. Attribute it to a comedy movie bad guy.',
     `Write a shout out to ${sample(initialProject === 'softstart' ? softstart : portrait)}`,
   ];
 
