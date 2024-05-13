0a32f4333904f88b438902e5e1e545cb76c4eafd
Victor Trinh <victor.trinh@gsoft.com>
2024-05-13 10:18:10 -0400

    Fix the prompt

--- src/pages/celebration/Celebration.tsx
+++ src/pages/celebration/Celebration.tsx
@@ -9,7 +9,6 @@ import Icon from '@mui/icons-material/Celebration';
 import useBoop from '@/hooks/use-boop.hook';
 import { faces } from '@/assets/default-sprites';
 import OpenAI from 'openai';
-import { sample } from '@/utils';
 import { ChatCompletion } from 'openai/resources';
 import { ConfettiGeyser } from './components';
 import { backgroundColors } from '../daily/utils/background-colors';
@@ -88,17 +87,6 @@ const Celebration = () => {
     }, 5000);
   };
 
-  // const prompts = [
-  //   // 'Generate a non existent random inspirational quote that will energize my team in Yoda style and attribute it to Yoda',
-  //   // 'Generate a non existent random inspirational quote that will energize my team in a pirate voice and attribute it to pirate character',
-  //   // 'Generate a non existent random inspirational quote that sounds like a quote from a sci-fi movie and attribute it to a character.',
-  //   // 'Generate a non existent random inspirational quote that will revolutionize the tech industry and attribute it to the God of tech',
-  //   // 'Generate a non existent random inspirational quote using metaphors and attribute it to a real athlete.',
-  //   // 'Generate a home repairs protip from a 90s movie and attribute it to the movie\'s character.',
-  //   // 'Start writing an inspirational quote but veer into unspeakable dark eldrich horror right before the end. Attribute it to a comedy movie bad guy.',
-  //   'Write a random technology fact of that happened on this day of the year',
-  // ];
-
   useEffect(() => {
     const fetchData = async () => {
       const result = await gf.search('celebrate', { sort: 'relevant', limit: 80 });
@@ -107,7 +95,7 @@ const Celebration = () => {
     };
 
     openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'Write a random technology fact of that happened on this day of the year' }],
+      messages: [{ role: 'user', content: 'Write a random tech fact that happened on the same day as today' }],
       model: 'gpt-3.5-turbo',
     }).then((response: ChatCompletion) => {
       const [firstChoice] = response.choices;
